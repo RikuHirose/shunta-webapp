9fc89d5d4ef5ac6bb7d286e521f8899c05038312
RikuHirose <rikuparkour9@gmail.com>
2019-06-07 22:40:54 +0900

    deploy

--- deploy.php
+++ deploy.php
@@ -48,4 +48,4 @@ after('deploy:failed', 'deploy:unlock');
 // Migrate database before symlink new release.
 // before('deploy:symlink', 'artisan:migrate');
 // before('deploy:symlink', 'artisan:admin:install');
-// before('deploy:symlink', 'artisan:db:seed');
+before('deploy:symlink', 'artisan:db:seed');

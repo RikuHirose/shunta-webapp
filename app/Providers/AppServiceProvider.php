3e736d2a2ad1723ca8272322fe484fc338db0cc6
RikuHirose <rikuparkour9@gmail.com>
2019-06-09 23:57:59 +0900

    production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -64,7 +64,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $is_production = env('APP_ENV') === 'production' ? true : false;
+        $is_production = env('APP_ENV') === 'prod' ? true : false;
         View::share('is_production',$is_production);
     }
 }
